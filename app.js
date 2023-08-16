25c2b72dd023d4de43fcd5e497cad25d31587698
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-16 11:40:01 -0700

    Question no 1 is complete

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// const yourname = document.querySelector("#yourname");
+// const email = document.querySelector("#email");
+// const password = document.querySelector("#password");
+// const login = document.querySelector("#login");
+
+// function loginUser(){
+//     console.log(yourname.value);
+//     console.log(email.value);
+//     console.log(password.value);
+// }
+
