8e70b4918e147695090639fb0608ba343a5ee796
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-18 10:33:12 -0700

    change

--- app.js
+++ app.js
@@ -11,58 +11,33 @@ function loginUser() {
 }
 
 // Q no 2
-<<<<<<< HEAD
-function calculate() {
-    const num1 = +document.querySelector("#num1").value;
-    const num2 = +document.querySelector("#num2").value;
-    const operator = document.querySelector("#operator").value;
-    const result = document.querySelector("#result")
-    result.style.border = "1px solid black"
-    result.style.width = "30%"
-    result.style.fontSize = "20px"
-    // console.log(num1,num2,operator)
-    switch (operator) {
-        case "+":
-            result.innerHTML = num1 + num2;
-            break
-        case "-":
-            result.innerHTML = num1 - num2;
-            break
-        case "*":
-            result.innerHTML = num1 * num2;
-            break
-        case "/":
-            result.innerHTML = num1 / num2;
-            break
-        default:
-            // console.log("Please Enter A Correct operator")
-            result.innerHTML = "Please Enter A Correct operator"
-    }
-}
-=======
-// function calculate (){
+// function calculate() {
 //     const num1 = +document.querySelector("#num1").value;
 //     const num2 = +document.querySelector("#num2").value;
 //     const operator = document.querySelector("#operator").value;
-//     // console.log(num1,num2,operator)
-//     switch(operator){
+//     const result = document.querySelector("#result")
+//     result.style.border = "1px solid black"
+//     result.style.width = "30%"
+//     result.style.fontSize = "20px"
+    // console.log(num1,num2,operator)
+//     switch (operator) {
 //         case "+":
-//             console.log(num1 + num2)
+//             result.innerHTML = num1 + num2;
 //             break
 //         case "-":
-//             console.log(num1 - num2)
+//             result.innerHTML = num1 - num2;
 //             break
 //         case "*":
-//             console.log(num1 * num2)
+//             result.innerHTML = num1 * num2;
 //             break
 //         case "/":
-//             console.log(num1 / num2)
+//             result.innerHTML = num1 / num2;
 //             break
 //         default:
-//             console.log("Please Enter A Correct operator")
+//             // console.log("Please Enter A Correct operator")
+//             result.innerHTML = "Please Enter A Correct operator"
 //     }
 // }
->>>>>>> c37193a5ceba9ed6b31f1b93acc0fe16ddaf77ee
 
 // Q No 3
 // function genrate() {
@@ -78,66 +53,65 @@ function calculate() {
 
 // }
 // QNo 3(02)
-const monthdropdon = document.querySelector("#monthdropdown");
-function genrater() {
-    const slectMonths = monthdropdon.value
-    const heading = document.querySelector("#head")
-    switch (slectMonths) {
-        case ("Jan"):
-            // console.log("Season is Winter")
-            heading.innerHTML = "Season is Winter"
-            break;
-        case ("Feb"):
-            // console.log("Season is Fall")
-            heading.innerHTML = "Season is Fall"
+// const monthdropdon = document.querySelector("#monthdropdown");
+// function genrater() {
+//     const slectMonths = monthdropdon.value
+//     const heading = document.querySelector("#head")
+//     switch (slectMonths) {
+//         case ("Jan"):
+//             // console.log("Season is Winter")
+//             heading.innerHTML = "Season is Winter"
+//             break;
+//         case ("Feb"):
+//             // console.log("Season is Fall")
+//             heading.innerHTML = "Season is Fall"
 
-            break;
-        case ("Mar"):
-            heading.innerHTML = "Season is Winter"
-            // console.log("Season is Winter")
-            break;
-        case ("Aprl"):
-            heading.innerHTML = "Season is Spring"
-            // console.log("Season is Spring")
-            break;
-        case ("May"):
-            heading.innerHTML = "Season is Winter"
-            // console.log("Season is Winter")
-            break;
-        case ("Jun"):
-            heading.innerHTML = "Season is Fall"
-            // console.log("Season is Fall")
-            break;
-        case ("July"):
-            heading.innerHTML = "Season is Spring"
-            // console.log("Season is Spring")
-            break;
-        case ("Aug"):
-            heading.innerHTML = "Season is Fall"
-            // console.log("Season is Fall")
-            break;
-        case ("Sep"):
-            heading.innerHTML = "Season is Winter"
-            // console.log("Season is Winter")
-            break;
-        case ("Oct"):
-            heading.innerHTML = "Season is Spring"
-            // console.log("Season is Spring")
-            break;
-        case ("Nove"):
-            heading.innerHTML = "Season is Fall"
-            // console.log("Season is Fall")
-            break;
-        case ("Dec"):
-            heading.innerHTML = "Season is Winter"
-            // console.log("Season is Winter")
-            break;
-        default:
-            heading.innerHTML = "Pleas Chose one"
-            // console.log("Pleas Chose one")
-    }
-}
-<<<<<<< HEAD
+//             break;
+//         case ("Mar"):
+//             heading.innerHTML = "Season is Winter"
+//             // console.log("Season is Winter")
+//             break;
+//         case ("Aprl"):
+//             heading.innerHTML = "Season is Spring"
+//             // console.log("Season is Spring")
+//             break;
+//         case ("May"):
+//             heading.innerHTML = "Season is Winter"
+//             // console.log("Season is Winter")
+//             break;
+//         case ("Jun"):
+//             heading.innerHTML = "Season is Fall"
+//             // console.log("Season is Fall")
+//             break;
+//         case ("July"):
+//             heading.innerHTML = "Season is Spring"
+//             // console.log("Season is Spring")
+//             break;
+//         case ("Aug"):
+//             heading.innerHTML = "Season is Fall"
+//             // console.log("Season is Fall")
+//             break;
+//         case ("Sep"):
+//             heading.innerHTML = "Season is Winter"
+//             // console.log("Season is Winter")
+//             break;
+//         case ("Oct"):
+//             heading.innerHTML = "Season is Spring"
+//             // console.log("Season is Spring")
+//             break;
+//         case ("Nove"):
+//             heading.innerHTML = "Season is Fall"
+//             // console.log("Season is Fall")
+//             break;
+//         case ("Dec"):
+//             heading.innerHTML = "Season is Winter"
+//             // console.log("Season is Winter")
+//             break;
+//         default:
+//             heading.innerHTML = "Pleas Chose one"
+//             // console.log("Pleas Chose one")
+//     }
+// }
 
 
 function changer() {
@@ -151,5 +125,3 @@ function changer() {
         para.style.color = "white"
     }
 }
-=======
->>>>>>> c37193a5ceba9ed6b31f1b93acc0fe16ddaf77ee
